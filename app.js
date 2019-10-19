8e3d6851f86a6f772aad4d02b1bf5bcba3ead1a3
Jun Kim <93kimhyunjun@gmail.com>
2019-10-19 12:02:20 +0900

    Edit package.json

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
 // catch 404 and forward to error handler
